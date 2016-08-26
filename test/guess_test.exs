c28d8f9728f19eb4cd63aeaca2963856b7a74f6d
Francisco Zanatta - 'Xikaos <francisco@organicadigital.com>
2016-08-25 23:06:25 -0300

    This last test broke it all, lol

--- test/guess_test.exs
+++ test/guess_test.exs
@@ -3,53 +3,17 @@ defmodule GuessTest do
   use ExUnit.Case
   doctest Guess
 
-  test "It Guess.div_helper ~> Doesnt always succeed" do
-    assert Guess.div_helper(0) != 1
+  test "It works with 0" do
+    assert Guess.guess(0,0..23) == 0
   end
 
-  test "It Guess.div_helper ~> works with 0" do
-    assert Guess.div_helper(0) == 0
+  test "It works with 1" do
+    assert Guess.guess(1,1..34) == 1
   end
 
-  test "Guess.div_helper ~> It works with 1" do
-    assert Guess.div_helper(1) == 1
-  end
-
-  test "Guess.div_helper ~> It works with 2" do
-    assert Guess.div_helper(2) == 1
-  end
-
-  test "Guess.div_helper ~> It works with 3" do
-    assert Guess.div_helper(3) == 2
-  end
-
-  test "Guess.div_helper ~> It really works" do
-    assert Guess.div_helper(1) == Guess.div_helper(2)
-  end
-
-  test "Guess.sum_helper ~> Doesnt always succeed" do
-    assert Guess.sum_helper(0,1) != 2
-  end
-
-  test "Guess.sum_helper ~> It works with 0 and 1" do
-    assert Guess.sum_helper(0,1) == 1
-  end
-
-  test "Guess.sum_helper ~> It works with 1 and 2" do
-    assert Guess.sum_helper(1,2) == 1
-  end
-
-  test "Guess.sum_helper ~> It works with 3 and 7" do
-    assert Guess.sum_helper(3,7) == 4
-  end
-
-  test "Guess.sum_helper ~> It works with 14 and 33" do
-    assert Guess.sum_helper(33,14) == 19
-  end
-
-  test "Guess.guess ~> It works with 13 between 0 and 20" do
-    IEx.pry
-    assert Guess.guess(13,0..20)
+  #This test Is giving me a lesson on how to deal with frustration
+  test "It works with 13" do
+    assert Guess.guess(13,0..42) == 13
   end
 
 end

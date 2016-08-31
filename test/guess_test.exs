d67d27fd1872369163ced5c82b1cb06e67b853c5
Francisco Zanatta - 'Xikaos <francisco@organicadigital.com>
2016-08-31 20:32:05 -0300

    Fix test

--- test/guess_test.exs
+++ test/guess_test.exs
@@ -1,19 +1,19 @@
 require IEx
 defmodule GuessTest do
   use ExUnit.Case
-  doctest Guess
+  doctest Chop
 
   test "It works with 0" do
-    assert Guess.guess(0,0..23) == 0
+    assert Chop.guess(0,0..23) == 0
   end
 
   test "It works with 1" do
-    assert Guess.guess(1,1..34) == 1
+    assert Chop.guess(1,1..34) == 1
   end
 
   #This test Is giving me a lesson on how to deal with frustration
   test "It works with 13" do
-    assert Guess.guess(13,0..42) == 13
+    assert Chop.guess(13,0..42) == 13
   end
 
 end
